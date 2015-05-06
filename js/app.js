1dda325d6ddbedfdb7fdff3898185f0038e5a03f
Collin Glass <collinglass@gmx.com>
2015-05-06 16:38:53 -0400

    comment out keigai code until we get preloader

--- js/app.js
+++ js/app.js
@@ -1,9 +1,109 @@
-var store = keigai.store();
+// var store = keigai.store();
  
-store.setUri("data.json").then(function (recs) {
-	riot.mount('narrative', {
-		items: recs
-	})
-}, function (err) {
-	// handle err
-});
+// store.setUri("data.json").then(function (recs) {
+	
+// }, function (err) {
+// 	// handle err
+// });
+
+riot.mount('narrative', {
+	items: [
+{ 
+	"title": "EARLY GREECE AND THE BRONZE AGE",
+	"description": "A long time ago..."
+},
+{ 
+	"title": "Odysseus",
+	"description": "The most charismatic cultural hero of the ancient Greeks",
+	"passage": "...saw the towns of many men and learned their minds, and suffered in his heart griefs upon the sea..."
+},
+{ 
+	"title": "Ancient Greeks were irresistibly drawn to distant shores",
+	"description": "from early in their history they ventured over the seas to foreign lands seeking their fortunes as traders, colonizers, and mercenary soldiers",
+	"time": { "year":-500, "relative": "before"}
+},
+{ 
+	"title": "The Greeks had planted colonies",
+	"description": "from Spain to the west coast of Asia and from the north Africa to the Black Sea",
+	"time": { "year":-500, "relative": "by" }
+},
+{ 
+	"title": "GREECE IN THE STONE AGES",
+	"description": "Humans entered Greece during the Middle Paleolithic (Old Stone) Age",
+	"time": { "year":-38000, "relative": "about" }
+},
+{ 
+	"title": "At the end of the Ice Age",
+	"description": "they hunted deer and smaller game, caught fish in the coastal waters, and gathered wild cereals, wild peas and beans, and nuts",
+	"time": { "year":-12000, "relative": "about" }
+},
+{ 
+	"title": "Early in the Neolithic (New Stone) Age",
+	"description": "the inhabitants began to cultivate the wild cereals and other plants, to domesticate animals, and to weave cloth on a loom",
+	"time": { "year":-6500, "relative": "about" }
+},
+{ 
+	"title": "Nomadic Tribe to settled villages",
+	"description": "agriculture cause people to create settlements and start to grow colonies",
+	"time": { "year":-6500, "relative": "about" }
+},
+{ 
+	"title": "The society of the small Stone Age villages",
+	"description": "probably egalitarian, no inequality outside of sex, age and skill",
+	"passage": "small temporary leaderships happend here and there when necessary to make decisions",
+	"time": { "year":-6500, "relative": "about" }
+},
+{ 
+	"title": "With the growth of the population",
+	"description": "a more lasting leadership emerged, termed by anthropologists as 'the big man', 'the head man', he who 'gets things done'",
+	"passage": "Henceforth, the division into two status groups, the very small group of leaders and the large group of the led...",
+	"time": { "year":-6500, "relative": "about" }
+},
+{ 
+	"title": "a permanent feature of Greek political life"
+},
+{ 
+	"title": "GREECE IN THE EARLY AND MIDDLE BRONZE AGES",
+	"description": "Bronze was discovered",
+	"passage": "By 2500 BC, Greece and the Balkans had mastered bronze, gold, lead, silver, and others",
+	"time": { "year":-3000, "relative": "between", "year2":-1600, }
+},
+{ 
+	"title": "The civilizations of the Near East",
+	"description": "Near east already higher level of organization of the natural and social environment termed 'civilization'",
+	"passage": "Aegean civilizations of Crete and Greece owe ther rise to the contact with the palace-kingdoms of the East"
+},
+{ 
+	"title": "Mesopotamia, Tigris and Euphrates appeared advanced civilization",
+	"description": "large-scale irrigation, cities with thousands of inhavitants, bureaucratic government, wide trade networks, written documents, legal systems, and science",
+	"passage": "no single political unit, there were discrete polities around major cities",
+	"time": { "year":-3500, "relative": "around" }
+},
+{ 
+	"title": "Egypt: Civilizations emerged",
+	"description": "same structure appeared here, although under a central governance by the Pharaoh",
+	"time": { "year":-3200, "relative": "around" }
+},
+{ 
+	"title": "Greece attained a fairly high level of social complexity",
+	"description": "Lerna and similar places had stone fortification walls and monumental buildings",
+	"time": { "year":-3000, "relative": "between", "year2":-2100, }
+},
+{ 
+	"title": "The first Greek-Speakers",
+	"description": "Incursion of a new people, who spoke an early form of Greek",
+	"time": { "year":-2100, "relative": "between", "year2":-1600, }
+},
+{ 
+	"title": "The Minoans",
+	"description": "Crete, a land of small city-kingdoms",
+	"time": { "year":-7000, "relative": "between", "year2":-1200, }
+},
+{ 
+	"title": "GREECE AND THE AEGEAN IN THE LATE BRONZE AGE",
+	"description": "Mycenean Greeks stole the wholesale model of the Cretan state to the writing system",
+	"passage": "Then went back, invaded and took over the Cretan palace-centers",
+	"time": { "year":-1600, "relative": "between", "year2":-1150, }
+}
+]
+})
--- narrative.tag
+++ narrative.tag
@@ -2,8 +2,8 @@
 
   <div each={ opts.items } class="space-frame" data-duration="2">
     <section class="space-inner-frame">
-      <h1>{ data.title }</h1>
-      <h3>{ data.description }</h3>
+      <h1>{ title }</h1>
+      <h3>{ description }</h3>
     </section>
   </div>
 
